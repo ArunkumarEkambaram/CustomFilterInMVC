c715ec6e3af86afb9f0b50dedd0292aa18811262
Arunkumar Ekambaram <earun2mail@gmail.com>
2019-05-13 17:43:07 +0530

    Add project files.

---
+++ CustomFilterInMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CustomFilterInMVC.MvcApplication" Language="C#" %>
---
+++ CustomFilterInMVC/Models/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CustomFilterInMVC.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() : base("HealthCare")
+        {
+        }
+    }
+}
